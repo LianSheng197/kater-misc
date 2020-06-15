2444232688c784a2b324f855060f5ab64217e845
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-06-15 11:34:49 +0800

    增加時間提示

--- rank/js/main.js
+++ rank/js/main.js
@@ -1,7 +1,14 @@
 // 導航欄訊息提示
 function msg(string){
+    let d = new Date();
+    let hh = `0${d.getHours()}`.substr(-2);
+    let mm = `0${d.getMinutes()}`.substr(-2);
+    let ss = `0${d.getSeconds()}`.substr(-2);
+    let time = `${hh}:${mm}:${ss}`;
+
     let msgArea = document.querySelector("nav#navbar div.right");
-    msgArea.innerText = string;
+    msgArea.innerText = `[${time}] ${string}`;
+    console.log(`[${time}] ${string}`);
 }
 
-
+msg("就緒");
