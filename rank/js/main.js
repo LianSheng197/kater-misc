171364d294b32da39783330e63331f673c897c68
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-06-22 22:36:09 +0800

    追加頁尾

--- rank/js/main.js
+++ rank/js/main.js
@@ -11,4 +11,21 @@ function msg(string){
     console.log(`[${time}] ${string}`);
 }
 
+function dateFormat(d){
+    let yyyy = d.getFullYear();
+    let MM = `0${d.getMonth()+1}`.substr(-2);
+    let dd = `0${d.getDate()}`.substr(-2);
+    let hh = `0${d.getHours()}`.substr(-2);
+    let mm = `0${d.getMinutes()}`.substr(-2);
+    let ss = `0${d.getSeconds()}`.substr(-2);
+    let datetime = `${yyyy}/${MM}/${dd} ${hh}:${mm}:${ss}`;
+    
+    return datetime;
+}
+
+
 msg("就緒");
+
+// footer
+document.querySelector("span#version").innerText = "v0.7.0";
+document.querySelector("span#lastModified").innerText = dateFormat(new Date(document.lastModified));
