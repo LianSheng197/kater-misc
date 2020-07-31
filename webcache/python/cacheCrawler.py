a4196f8f1a48b95af5157b6505a5d0cce309951e
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-07-31 16:52:08 +0800

    完成

--- webcache/python/cacheCrawler.py
+++ webcache/python/cacheCrawler.py
@@ -5,17 +5,32 @@ import sys
 import requests
 import signal
 import yaml
-from time import sleep
+import time
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from pathlib import Path
 from random import randint
 
 
-def crawler(discussionStart, discussionEnd, UA):
-    for i in range(discussionStart, discussionEnd):
-        print(f"\r編號 {i}: ", end='')
+def crawler(dmin, dmax, wmin, wmax, emin, emax, UA):
+    """
+        討論串編號      dmin ~ dmax
+        例常等待間隔    wmin ~ wmax
+        意外等待間隔    emin ~ emax
+    """
+    print(UA)
+    logger(UA)
+
+    # 為了讓 429 能夠重試，只好改用這種寫法
+    i = dmin
+    while i < dmax:
+        logtemp = ""
+        now = str(time.strftime("%m/%d %H:%M:%S", time.localtime()))
+        print(f"\r[{now}] 編號 {i}: ", end='')
+        logtemp += f"\r[{now}] 編號 {i}: "
+
         headers = {
-            "User-Agent": ""
+            "User-Agent": UA
         }
         Html = f"http://webcache.googleusercontent.com/search?q=cache:https://kater.me/d/{str(i)}"
 
@@ -25,76 +40,139 @@ def crawler(discussionStart, discussionEnd, UA):
         status = res.status_code
 
         print(f"[{status}] ", end='')
+        logtemp += f"[{status}] "
 
         soup = BeautifulSoup(res.text, 'html.parser')
 
         # 只存有內容的快取
-        if(status == 200):
-            filename = f"./d/{str(i)}.html"
-            print(f"({filename}) ", end='')
-            with open(filename, 'w', encoding="utf-8") as file:
-                file.write(soup.prettify())
+        if status in [200, 404]:
+            if(status == 200):
+                filename = f"./d/{str(i)}.html"
+                print(f"({filename}) ", end='')
+                logtemp += f"({filename}) "
 
-        s = randint(6789, 23456) / 1000
-        print(f"等待請求間隔 {s} 秒")
-        waiting(s)
+                with open(filename, 'w', encoding="utf-8") as file:
+                    file.write(soup.prettify())
+            else:
+                print(f"(狀態 404 不存檔) ", end='')
+                logtemp += f"(狀態 404 不存檔) "
+
+            s = randint(wmin, wmax) / 1000
+            print(f"等待請求間隔 {s} 秒")
+            logtemp += f"等待請求間隔 {s} 秒"
+            logger(logtemp)
+
+            waiting(s)
+        else:
+            if(status == 429):
+                print(f"(狀態 429 更換 User-Agent 並等待一段時間) ", end='')
+                s = randint(emin, emax) / 1000
+                print(f"等待請求間隔 {s} 秒")
+
+                logtemp += f"(狀態 429 更換 User-Agent 並等待一段時間) 等待請求間隔 {s} 秒"
+                logger(logtemp)
+
+                UA = ua.random
+                print(UA)
+                logger(UA)
+
+                waiting(s)
+                i -= 1
+            else:
+                print(f"(非預期狀態，目前版本不採取動作)", end='')
+                s = randint(wmin, wmax) / 1000
+                print(f"等待請求間隔 {s} 秒")
+
+                logtemp += f"(非預期狀態，目前版本不採取動作) 等待請求間隔 {s} 秒"
+                logger(logtemp)
+
+                waiting(s)
+
+        i += 1
 
 
 def handler(sig, frame):
-    print("\r程式已結束")
+    print("\r程式已被使用者中止")
+    logger("\n程式已被使用者中止\n\n")
     sys.exit(0)
 
 
-def waiting(time):
-    while(time > 0):
-        print(f"\r{'{:.3f}'.format(time)}", end='')
-        time -= 0.033
-        sleep(0.033)
+def waiting(remaining):
+    while(remaining > 0):
+        print(f"\r{'{:.3f}    '.format(remaining)}", end='')
+        remaining -= 0.033
+        time.sleep(0.033)
+
+
+def logger(log):
+    with open("log.txt", 'a', encoding="utf-8") as file:
+        file.write(log)
 
 
 def checkConfig():
+    print("正在讀取設定檔...")
+    logger("正在讀取設定檔...\n")
     with open("config.yml", 'r', encoding="utf-8") as stream:
         try:
             _config = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print("讀取設定檔失敗！")
-            print(exc)
+        except yaml.YAMLError as error:
+            print("讀取設定檔失敗！程式已終止。")
+            logger("讀取設定檔失敗！程式已終止。\n\n")
+            print(error)
+            input()
             sys.exit(255)
 
     print("正在檢查設定檔格式")
+    logger("正在檢查設定檔格式\n")
 
-    if(_config["token"] == None):
-        print("設定錯誤：未填入 token")
+    try:
+        dmin = _config["range"]["min"] + 0
+        dmax = _config["range"]["max"] + 0
+        wmin = _config["interval"]["routine"]["min"] + 0
+        wmax = _config["interval"]["routine"]["max"] + 0
+        emin = _config["interval"]["exception"]["min"] + 0
+        emax = _config["interval"]["exception"]["max"] + 0
+    except error:
+        print("檢查格式失敗，請確定格式與 config.example.yml 一致，並且皆爲數值。程式已終止。")
+        logger("檢查格式失敗，請確定格式與 config.example.yml 一致，並且皆爲數值。程式已終止。\n\n")
+        print(error)
         input()
-        sys.exit(201)
-    if(not isinstance(_config["report"]["period"], int) or _config["report"]["period"] < 30):
-        print("設定錯誤： report.period 必須是正整數，且不能小於 30")
-        input()
-        sys.exit(202)
-    if(not isinstance(_config["report"]["warningLevel"], int) or _config["report"]["warningLevel"] < 1 or _config["report"]["warningLevel"] > 3):
-        print("設定錯誤： report.warningLevel 必須是 1, 2, 3 其一")
-        input()
-        sys.exit(203)
+        sys.exit(254)
+
+    print(f"已讀取設定：\n    討論編號     {dmin} ~ {dmax}\n    例常等待時間 {wmin / 1000} ~ {wmax / 1000} 秒\n    例外等待時間 {emin / 1000} ~ {emax / 1000} 秒\n")
+    logger(f"已讀取設定：\n    討論編號     {dmin} ~ {dmax}\n    例常等待時間 {wmin / 1000} ~ {wmax / 1000} 秒\n    例外等待時間 {emin / 1000} ~ {emax / 1000} 秒\n\n")
+    return [dmin, dmax, wmin, wmax, emin, emax]
 
 
 # ----------------------- Main ----------------------- #
+logger("--------------------------------\n\n")
+now = str(time.strftime("%m/%d %H:%M:%S", time.localtime()))
+print(now)
+logger(f"{now} 程式開始\n\n")
+
 # 攔截終止訊號
 signal.signal(signal.SIGINT, handler)
 
 # 建立資料夾 d
 Path("./d").mkdir(parents=True, exist_ok=True)
 
+# 從檔案取得 UA
+ua = UserAgent(path="./fake-useragent.json")
+
 # 檢查 config.yml 是否存在
-if Path("./config1.yml").is_file():
-    print()
+if Path("./config.yml").is_file():
+    config = checkConfig()
 else:
     print("找不到設定檔 config.yml，程式已終止。")
+    logger("找不到設定檔 config.yml，程式已終止。\n\n")
     input()
     sys.exit(100)
 
 # （主要）自動爬取檔案
-crawler(80000, 80002)
+crawler(config[0], config[1], config[2],
+        config[3], config[4], config[5], ua.random)
 
 # 程式完成
-print("\r已完成！按 ENTER 關閉程式")
+print("\r        \n已完成！按 ENTER 關閉程式")
+logger("\n已完成！\n\n")
 input()
