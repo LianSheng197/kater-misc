0229597256a45c6e44fc944f4afe43ca4831fdc1
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-08-04 01:17:52 +0800

    translator: pre, code

--- webcache/python/translator.py
+++ webcache/python/translator.py
@@ -33,61 +33,106 @@ def getImageUrl(element):
     elif(element.name == "a"):
         return getChildrenList(element)[0]["src"]
 
+
 def nospace(string):
     string = re.sub(r"^\ +", "", string)
     string = re.sub(r"\ +$", "", string)
     return string
 
-def DFSParser(element):
+
+def DFSParser(element, noBreakLine=True):
     global md
 
+    # 沒有子節點
     if not hasChildren(element):
         if isinstance(element, Tag):
-            md += f"{nospace(element.text)}"
+            content = nospace(element.text)
+            if noBreakLine:
+                content = content.replace("\n", "")
+                md += f"{content}"
+            else:
+                md += f"{content}"
         else:
-            md += nospace(element)
+            content = nospace(element)
+            if noBreakLine:
+                content = content.replace("\n", "")
+                md += f"{content}"
+            else:
+                md += f"{content}"
         return
+    # 有子節點
     else:
         for e in getChildrenList(element):
-            if isinstance(element, Tag):
-                if(isLinkImage(e)):
-                    md += f"![]({getImageUrl(e)})"
-                elif(e.name == "a"):
-                    if(e.has_attr("class") and e["class"][0] == "PostMention"):
-                        md += f"[回覆：{e.text}](#)\n"
-                    else:
-                        md += f"[{e.text}]({getLinkUrl(e)})"
-                elif(e.name == "p"):
-                    DFSParser(e)
-                    md += "\n\n"
-                elif(e.name == "iframe"):
-                    if e.has_attr("src"):
-                        src = e["src"]
-
-                        if("www.youtube.com" in src):
-                            src = src.replace("embed/", "watch?v=")
-                            md += src
-                        elif("twitter.min.html" in src):
-                            # 推特原連結被 s9e 格式化了。僅知道 id 的狀況，不曉得如何反推原始連結
-                            md += src
+            # e 為節點
+            if isinstance(e, Tag):
+                if e.name not in ["script", "style"]:
+                    if(isLinkImage(e)):
+                        md += f"![]({getImageUrl(e)})"
+                    elif(e.name == "a"):
+                        if(e.has_attr("class") and e["class"][0] == "PostMention"):
+                            content = nospace(e.text).replace("\n", "")
+                            md += f"[回覆：{content}](#)\n"
                         else:
-                            md += src
+                            content = nospace(e.text).replace("\n", "")
+                            md += f"[{content}]({getLinkUrl(e)})"
+                    elif(e.name == "p"):
+                        DFSParser(e)
+                        md += "\n\n"
+                    elif(e.name == "iframe"):
+                        if e.has_attr("src"):
+                            src = e["src"]
 
-                elif(e.name == "blockquote"):
-                    md += "[quote]\n"
-                    DFSParser(e)
-                    md += "[/quote]\n\n"
-                elif(e.name == "br"):
-                    md += "\n"
-                elif(e.name == "strong"):
-                    md += "**"
-                    DFSParser(e)
-                    md += "**"
-                else:
-                    DFSParser(e)
-            
+                            if("www.youtube.com" in src):
+                                src = src.replace("embed/", "watch?v=")
+                                md += src
+                            elif("twitter.min.html" in src):
+                                # https://twitter.com/x/status/:id
+                                id = src.split("#")[1]
+                                md += f"https://twitter.com/x/status/{id}"
+                            elif("facebook.min.html" in src):
+                                #
+
+                                md += src
+                            else:
+                                md += src
+
+                            md += "\n"
+
+                    elif(e.name == "blockquote"):
+                        md += "[quote]\n"
+                        DFSParser(e)
+                        md += "[/quote]\n\n"
+                    elif(e.name in ["code", "pre"]):
+                        if(e.name == "pre"):
+                            DFSParser(e, noBreakLine=False)
+                        else:
+                            if(e.parent.name == "pre"):
+                                if(e.has_attr("class")):
+                                    lang = e["class"][0].split("-")[1]
+                                    md += f"```{lang}\n"
+                                    DFSParser(e, noBreakLine=False)
+                                    md += "\n```\n"
+                            else:
+                                md += f" `"
+                                DFSParser(e)
+                                md += "` "
+                    elif(e.name == "br"):
+                        md += "\n"
+                    elif(e.name == "strong"):
+                        md += "**"
+                        DFSParser(e)
+                        md += "**"
+                    else:
+                        DFSParser(e)
+
+            # e 不是節點（只是文字）
             else:
-                md += nospace(element)
+                content = nospace(e)
+                if noBreakLine:
+                    content = content.replace("\n", "")
+                    md += f"{content}"
+                else:
+                    md += f"{content}"
 
 
 originHtml = "./d"
@@ -108,7 +153,7 @@ if not Path(f"./translated/json/{filenames[index]}").is_file():
     raw = ""
     # with open(f"{originHtml}/{filenames[index]}.html", "r", encoding="utf-8") as file:
     # 僅測試用
-    with open(f"{originHtml}/60866.html", "r", encoding="utf-8") as file:
+    with open(f"{originHtml}/71476.html", "r", encoding="utf-8") as file:
         raw = file.read()
 
     soup = BeautifulSoup(raw, "html.parser")
@@ -128,7 +173,7 @@ if not Path(f"./translated/json/{filenames[index]}").is_file():
     while(index < len(postAuthor)):
         body = postBody[index]
 
-        body = body.decode_contents().replace("\n", "")
+        body = body.decode_contents()
         body = re.sub(r"\ +", " ", body)
         author = postAuthor[index]
         author = author.text.replace(" ", "").replace("\n", "")
